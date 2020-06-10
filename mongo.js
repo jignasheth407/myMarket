daf1ab66bd0265b2dd56fe31cc14cd29b0c481cb
jignasheth407 <jignasheth407@gmail.com>
2020-06-10 10:44:31 +0530

    change mongodb connection

--- mongo.js
+++ mongo.js
@@ -1,16 +1,16 @@
-// const mongoose = require("mongoose");
-
-// require("dotenv").config();
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGOURI, { useCreateIndex: true, useNewUrlParser: true });
-
-
 const mongoose = require("mongoose");
+
 require("dotenv").config();
 mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGOURI, { useCreateIndex: true, useNewUrlParser: true });
 
-const MONGOLAB_URI = "mongodb+srv://admin:admin@123@cluster0-b8bnt.mongodb.net/binanceDB?retryWrites=true&w=majority"
 
-mongoose.connect(MONGOLAB_URI, {useCreateIndex: true, useNewUrlParser: true})
-.then(() => console.log('Mongodb connected.'))
-.catch(e => console.log(e));
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// mongoose.Promise = global.Promise;
+
+// const MONGOLAB_URI = "mongodb+srv://admin:admin@123@cluster0-b8bnt.mongodb.net/binanceDB?retryWrites=true&w=majority"
+
+// mongoose.connect(MONGOLAB_URI, {useCreateIndex: true, useNewUrlParser: true})
+// .then(() => console.log('Mongodb connected.'))
+// .catch(e => console.log(e));
