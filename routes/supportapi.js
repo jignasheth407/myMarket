d334997dfd5aec9c8e9ec62d3530a7674f1e317c
jignasheth407 <jignasheth407@gmail.com>
2020-06-13 13:09:16 +0530

    update add product

--- routes/supportapi.js
+++ routes/supportapi.js
@@ -36,25 +36,7 @@ var storage = multer.diskStorage({
     },
 });
 
-const fileFilter = (req, file, cb) => {
-  
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png")
-    {
-        cb(null, true);
-    } 
-    else
-    {
-        cb(null, false);
-    }
-};
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
-  fileFilter: fileFilter,
-});
+const upload = multer({ storage: storage });
 
 /* model */
 const Users = mongoose.model("Users");
